1f1335f9c767eb927981030effbbbdc242a64f9b
Juanito Ortega <ortegaj83@gmail.com>
2022-05-07 11:56:01 -0400

    first commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+Curso: https://platzi.com/cursos/react/
+
+# Creamos nuestra app con el modo rápido
+npx create-react-app .
+
+# Instalamos sass
+npm install sass
+
+# Lanzar servidor de desarrollo
+npm start
+
+# Para hacer el deploy en gh pages instalamos la siguiente librería en modo desarrollador
+npm install --save-dev gh-pages
---
+++ src/TodoForm/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext';
+import './TodoForm.css';
+
+function TodoForm() {
+  const [newTodoValue, setNewTodoValue] = React.useState('');
+  const {
+    addTodo,
+    setOpenModal,
+  } = React.useContext(TodoContext);
+  
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
+  };
+  const onCancel = () => {
+    setOpenModal(false);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (!newTodoValue.trim()) {
+      // alert("El nombre está vacío, escribe algo");
+      return;
+    }
+    addTodo(newTodoValue);
+    setOpenModal(false);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label>Escribe tu nuevo TODO</label>
+      <textarea
+        value={newTodoValue}
+        onChange={onChange}
+        placeholder="Cortar la cebolla oara el almuerzo"
+      />
+      <div className="TodoForm-buttonContainer">
+        <button
+          type="button"
+          className="TodoForm-button TodoForm-button--cancel"
+          onClick={onCancel}
+          >
+          Cancelar
+        </button>
+        <button
+          type="submit"
+          className="TodoForm-button TodoForm-button--add"
+        >
+          Añadir
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export { TodoForm };
